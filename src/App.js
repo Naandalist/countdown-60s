8d4123ea08df011daa339ad1204ccffc1fcfcf2a
“naandalist” <listiananda.apriliawan@gmail.com>
2023-01-18 01:00:06 +0700

    style: update layout

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
 
 function App() {
+  const [seconds, setSeconds] = useState(60);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      if (seconds > 0) {
+        setSeconds((seconds) => seconds - 1);
+      }
+    }, 1000);
+    
+    return () => clearInterval(interval);
+  }, [seconds]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="center">
+      <h1 style={{ animation: "blink 1s linear infinite" }}>
+        {seconds > 0 ? `${seconds} seconds` : `Time is Up!`}
+      </h1>
     </div>
   );
 }
